2eacd5af25e15774cd21d613325aa1a3f2007d18
oluwakoredeogundele <92125132+oluwakoredeogundele@users.noreply.github.com>
2022-06-25 15:54:10 +0100

    first

---
+++ script.js
@@ -0,0 +1,12 @@
+// variables defined;
+const lastName = "Samuel";
+const surname ="Ogundele" ;
+const firstName = 'Folarin';
+ const fullName =`Name \n ${surname} ${firstName} ${lastName}` ;
+ const height = ' Height \n 1.77 Meters' ;
+ const country = ' Country  \n Nigeria';
+
+// three alerts were used
+ alert(fullName);
+ alert(height);
+ alert(country);
